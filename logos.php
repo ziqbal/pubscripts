b9139e563978388949dcedb224347edb31ef27bb
Zaf <ultrasine@gmail.com>
2016-02-14 20:19:52 +0200

    logos up

---
+++ logos.php
@@ -0,0 +1,146 @@
+<?php
+
+
+//if( count( $argv ) < 2 ) { print( "More args!" ) ; exit ; }
+
+$_CONFIG = array( ) ;
+$_CONFIG[ "originalargs" ] = $argv ;
+
+/*
+if(!isset($_CONFIG["originalargs"][2])){
+  print("project name required!");
+  exit;
+}
+*/
+
+$_CONFIG[ "targetdir" ] = $argv[ 1 ] ;
+//print_r( $_CONFIG ) ; exit ;
+
+/////
+
+$grid=array();
+for($x=0;$x<1024;$x++){
+  for($y=0;$y<1024;$y++){
+    $grid[$x][$y]=-1;
+  }
+}
+
+$sw=20;
+$sh=20;
+
+$cx = 0 ;
+$cy = 0 ;
+$updateCursor=true;
+$chighlight=1;
+
+$mode='command';
+
+readline_callback_handler_install( '' , function( ) { } ) ;
+
+system('tput clear');
+
+$frame = 0 ;
+
+while( true ) {
+
+  $r = array( STDIN ) ;
+  $w = NULL ;
+  $e = NULL ;
+
+  $n = stream_select( $r , $w , $e , 0 ) ;
+
+  if( $n && in_array( STDIN , $r ) ) {
+
+	system( 'echo "lines\ncols" | tput -S > /tmp/tput.dat' ) ;
+
+	//system('tput clear');
+//	system('tput cup 0 0');
+    $c = stream_get_contents( STDIN , 1 ) ;
+
+    //echo "Char read: [$c] [".ord($c)."]\n" ;
+
+    //$f=file_get_contents("/tmp/tput.dat");
+   	//print_r($f); 
+
+    if($mode=='command'){
+
+    if( $c == 'j' ){
+      $cx-- ;
+      if($cx<0) $cx = 0 ;
+      $updateCursor=true;
+    }
+    if( $c == 'k' ){
+      $cx++ ;
+      if($cx>$sw) $cx = $sw ;
+      $updateCursor=true;
+    }
+    if( $c == 'd' ){
+      $cy-- ;
+      if($cy<0) $cy = 0 ;
+      $updateCursor=true;
+    }
+    if( $c == 'f' ){
+      $cy++ ;
+      if($cy>$sh) $cy = $sh ;
+      $updateCursor=true;
+    }
+    if( $c == 'q' ) break ;
+    if(ord($c)==27){
+      $mode="edit";
+    }    
+      continue;
+
+    }
+
+
+    if(ord($c)==27){
+      $mode="command";
+      $updateCursor=true;
+      continue;
+    }
+
+    if($mode=='edit'){
+      print("$c");
+      system('tput cnorm');
+    }
+
+
+  }
+
+  if($updateCursor){
+    system("tput cup $cy $cx");
+      system('tput cnorm');
+    //system("tput setb 2");
+    //print("*");
+    //system("tput cup $cy $cx");
+    $updateCursor=false;
+
+  }
+
+  $frame++;
+
+  if($mode=='edit'){
+    $cursorFlashTrigger=500000;
+  }else{
+    $cursorFlashTrigger=100000;
+
+  }
+
+  if(($frame%$cursorFlashTrigger)==0){
+    if($chighlight==1){
+      //system('tput setb 4');
+      system('tput cnorm');
+    }else{
+      //system('tput setb 0');
+      system('tput civis');
+    }
+    $chighlight*=-1;
+    //print(".");
+  }
+
+}
+
+system('tput clear');
+system('tput cnorm');
+system("tput sgr0");
+
---
+++ logos.sh
@@ -0,0 +1,19 @@
+#
+# My Own Editor!
+# Parameter 1 = filename
+#
+
+INTERPRETER="/usr/bin/php"
+#INTERPRETER="/Applications/XAMPP/bin/php"
+
+STARTDIR=$(pwd)
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILENAME=$(basename "${BASH_SOURCE[0]}")
+EXTENSION="${FILENAME##*.}"
+FNAME="${FILENAME%.*}"
+
+cd $SCRIPTDIR
+
+$INTERPRETER $FNAME.php $STARTDIR "$@"
+
